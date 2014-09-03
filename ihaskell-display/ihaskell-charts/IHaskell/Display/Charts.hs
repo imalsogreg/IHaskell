971dca44e026ab3c256cdc32399e6da7b0b0456d
Greg Hale <imalsogreg@gmail.com>
2014-09-03 17:26:35 -0400

    Add method for sizing charts

--- ihaskell-display/ihaskell-charts/IHaskell/Display/Charts.hs
+++ ihaskell-display/ihaskell-charts/IHaskell/Display/Charts.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-module IHaskell.Display.Charts () where
+module IHaskell.Display.Charts (sizeRenderable) where
 
 import ClassyPrelude
 
@@ -12,33 +12,42 @@ import System.IO.Unsafe
 
 import IHaskell.Display
 
-width :: Width
-width = 450
-
-height :: Height
-height = 300
-
 instance IHaskellDisplay (Renderable a) where
   display renderable = do
-    pngDisp <- chartData renderable PNG
+    
+    let (defaultWid, defaultHeight) = (450,300)
+
+    pngDisp <- chartData defaultWid defaultHeight renderable PNG
 
     -- We can add `svg svgDisplay` to the output of `display`,
     -- but SVGs are not resizable in the IPython notebook.
-    svgDisp <- chartData renderable SVG
+    svgDisp <- chartData 450 300 renderable SVG
 
     return $ Display [pngDisp, svgDisp]
 
-chartData :: Renderable a -> FileFormat -> IO DisplayData
-chartData renderable format = do
+chartData :: Width -> Height -> Renderable a -> FileFormat -> IO DisplayData
+chartData w h renderable format = do
   switchToTmpDir
 
   -- Write the PNG image.
-  let filename = ".ihaskell-chart.png" :: String
-      opts = def{_fo_format = format, _fo_size = (width, height)}
-  renderableToFile opts filename renderable
+  let tmpFile = ".ihaskell-chart.png"
+      opts = def{_fo_format = format, _fo_size = (w,h)}
+  renderableToFile opts tmpFile renderable
 
   -- Convert to base64.
-  imgData <- readFile $ fpFromString filename
+  imgData <- readFile $ fpFromString tmpFile
   return $ case format of
-    PNG -> png width height $ base64 imgData
+    PNG -> png w h $ base64 imgData
     SVG -> svg $ Char.unpack imgData
+
+data SizedRenderable a = SizedRenderable Width Height (Renderable a)
+
+sizeRenderable :: (Renderable a) -> Width -> Height -> SizedRenderable a
+sizeRenderable r w h = SizedRenderable w h r
+
+instance IHaskellDisplay (SizedRenderable a) where
+  display (SizedRenderable h w renderable) = do
+    pngDisp <- chartData w h renderable PNG
+    svgDisp <- chartData w h renderable SVG
+    return $ Display [pngDisp, svgDisp]
+
